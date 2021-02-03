7adcd353e3a5cddc2843035d5f3f251c476fde4a
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-03 08:48:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h3 data-ns-test="project-name">React JS</h3>
+    <p data-ns-test="project-description">I am learning React JS</p>
+    </div>
   )
 }
 
